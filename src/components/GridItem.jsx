efd2704f7ef45fe91f95bc2d2fcbb1609c3a2ad6
daviardev <jerson.silva99@hotmail.com>
2023-11-11 19:27:47 -0500

    Fix somethings

--- src/components/GridItem.jsx
+++ src/components/GridItem.jsx
@@ -4,44 +4,39 @@ import { Box, Text, LinkBox, LinkOverlay, Image } from '@chakra-ui/react'
 
 import { Global } from '@emotion/react'
 
-export const GridItem = ({ children, href, title, thumbnail }) => {
-  return (
-    <>
-      <Box
-        w='100%'
-        align='center'
+export const GridItem = ({ children, href, title, thumbnail }) => (
+  <Box
+    w='100%'
+    align='center'
+  >
+    <LinkBox
+      cursor='pointer'
+    >
+      <Image
+        src={thumbnail}
+        alt={title}
+        className='grid-item-thumbnail'
+        placeholder='blur'
+        loading='lazy'
+      />
+      <LinkOverlay
+        href={href}
+        target='_blank'
       >
-        <LinkBox
-          cursor='pointer'
+        <Text
+          mt={2}
         >
-          <Image
-            src={thumbnail}
-            alt={title}
-            className='grid-item-thumbnail'
-            placeholder='blur'
-            loading='lazy'
-          >
-            <LinkOverlay
-              href={href}
-              target='_blank'
-            >
-              <Text
-                mt={2}
-              >
-                {title}
-              </Text>
-            </LinkOverlay>
-            <Text
-              fontSize={14}
-            >
-              {children}
-            </Text>
-          </Image>
-        </LinkBox>
-      </Box>
-    </>
-  )
-}
+          {title}
+        </Text>
+      </LinkOverlay>
+      <Text
+        fontSize={14}
+      >
+        {children}
+      </Text>
+    </LinkBox>
+  </Box>
+)
 
 export const WorkGridItem = ({ children, id, title, thumbnail }) => {
   return (
